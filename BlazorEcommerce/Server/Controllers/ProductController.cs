a2991f273d13e7ab5d70f327ac5b9f3a25a60728
Ebenezer Ahurein <ahureinebenezer@gmail.com>
2023-07-13 13:40:11 +0000

    Add project files.

---
+++ BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorEcommerce.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        private static List<Product> Products = new List<Product>();
+        private readonly IProductService _products;
+
+        public ProductController(IProductService products)
+        {
+            _products = products;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProducts()
+        {
+            var products = await _products.GetProductsAsync();
+            return Ok(products);
+        }
+    }
+}
